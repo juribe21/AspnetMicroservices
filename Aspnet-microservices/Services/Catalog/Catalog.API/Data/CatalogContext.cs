4e4ae5ea9f360c3ce1a7ce8db95be1765427719d
Jorge Uribe <jorgeuribe21@gmail.com>
2021-08-23 19:42:29 -0700

    Catalog.Api created Context, Entities and appsettings

---
+++ Aspnet-microservices/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration )
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabasSettigs:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabasSettigs:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabasSettigs:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
